fba07d483c2f31a6f3c9404aeaf0eff8c3787d90
Amulya Jain <amuly.jain@gmail.com>
2023-03-04 23:26:25 -0500

    update

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ from os import path
 import subprocess
 from pathlib import Path
 import dload
-from tkinter import Tk     # from tkinter import Tk for Python 3.x
+from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 path1 = os.path.dirname(os.path.realpath(__file__))
 if not (path.exists(path1+"\\realesrgan-ncnn-vulkan-20220424-windows\\realesrgan-ncnn-vulkan.exe")):
 	print("Downloding")
 	dload.save_unzip( """https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.5.0/realesrgan-ncnn-vulkan-20220424-windows.zip""")
 	os.remove(path1+"\\realesrgan-ncnn-vulkan-20220424-windows.zip")		
-model_name="realesrgan-x4plus" 			# realesr-animevideov3 | realesrgan-x4plus | realesrgan-x4plus-anime
+model_name="realesrgan-x4plus"
 anime=input("""Is your image anime?
-y/n""")
+y/n """)
 if anime.lower() in ['y','ye','yes','yess']:
     model_name="realesrgan-x4plus-anime"
 Tk().withdraw()
